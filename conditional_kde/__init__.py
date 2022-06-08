76697aca647fb1aacdbc4d69e613c97592ad4eed
David Prelogovic <david.prelogovic@gmail.com>
2022-06-08 14:13:47 +0200

    small change

--- conditional_kde/__init__.py
+++ conditional_kde/__init__.py
@@ -4,10 +4,8 @@ __author__ = """David Prelogović"""
 __email__ = "david.prelogovic@gmail.com"
 __version__ = "0.1.0"
 
-from .gaussian import (
-    ConditionalGaussianKernelDensity,
-    InterpolatedConditionalKernelDensity,
-)
+from .gaussian import ConditionalGaussianKernelDensity
+from .interpolated import InterpolatedConditionalKernelDensity
 
 __all__ = [
     "ConditionalGaussianKernelDensity",
