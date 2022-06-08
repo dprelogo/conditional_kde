468d5120f14f720631d2a2e85c2f3cf8abd1f476
David Prelogovic <david.prelogovic@gmail.com>
2022-06-08 11:25:37 +0200

    add interpolation class

--- conditional_kde/__init__.py
+++ conditional_kde/__init__.py
@@ -1,9 +1,15 @@
 """Top-level package for Conditional KDE."""
 
 __author__ = """David Prelogović"""
-__email__ = 'david.prelogovic@gmail.com'
-__version__ = '0.1.0'
+__email__ = "david.prelogovic@gmail.com"
+__version__ = "0.1.0"
 
-from .conditional_kde import ConditionalKernelDensity
+from .conditional_kde import (
+    ConditionalGaussianKernelDensity,
+    InterpolatedConditionalKernelDensity,
+)
 
-__all__ = ["ConditionalKernelDensity"]
+__all__ = [
+    "ConditionalGaussianKernelDensity",
+    "InterpolatedConditionalKernelDensity",
+]
